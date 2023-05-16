8c239b8352ccb23b528cff8c7fcaeb0526efb1cd
rongjian <rongjianxiao@gmail.com>
2023-05-16 14:32:43 -0700

    edit function

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -7,6 +7,7 @@
       show-checkbox
       node-key="catId"
       :default-expanded-keys="expandedKey"
+      draggable
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -19,6 +20,9 @@
           >
             Append
           </el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            Edit
+          </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -30,15 +34,24 @@
         </span>
       </span>
     </el-tree>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog :title="title" :visible.sync="dialogVisible" width="30%">
       <el-form :model="category">
         <el-form-item label="Category">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="Icon">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Unit">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -54,7 +67,9 @@ export default {
   props: {},
   data () {
     return {
-      category: { name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      title: '',
+      dialogType: '',
+      category: { name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0, icon: '', productUnit: '' },
       dialogVisible: false,
       menus: [],
       expandedKey: [],
@@ -74,15 +89,33 @@ export default {
         this.menus = data.data
       })
     },
+
+    submitData () {
+      if (this.dialogType === 'add') {
+        this.addCategory()
+      }
+      if (this.dialogType === 'edit') {
+        this.editCategory()
+      }
+    },
+
     append (data) {
       console.log('Append Data: ', data)
       this.dialogVisible = true
+      this.dialogType = 'add'
+      this.title = 'Add'
       this.category.parentCid = data.catId
       this.category.catLevel = data.catLevel + 1
+      this.category.name = ''
+      this.category.catId = null
+      this.category.icon = ''
+      this.productUnit = ''
+      this.category.sort = ''
+      this.category.showStatus = 1
     },
 
     addCategory () {
-      console.log('Category Data: ', this.category)
+      console.log('Add Category Data: ', this.category)
       this.$http({
         url: this.$http.adornUrl('/product/category/save'),
         method: 'post',
@@ -90,6 +123,53 @@ export default {
       }).then(({ data }) => {
         this.$message({
           message: 'Add Successfully',
+          type: 'success'
+        })
+        // Close dialog
+        this.dialogVisible = false
+        // Refresh for new menus
+        this.getMenus()
+        // Set default menu
+        this.expandedKey = [this.category.parentCid]
+      })
+    },
+
+    edit (data) {
+      console.log('Edit data: ', data)
+      this.dialogVisible = true
+      this.dialogType = 'edit'
+      this.title = 'Edit'
+
+      // request for latest data
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get'
+      }).then(({ data }) => {
+        console.log('return data: ', data)
+        this.category.name = data.category.name
+        this.category.catId = data.category.catId
+        this.category.icon = data.category.icon
+        this.category.productUnit = data.category.productUnit
+        this.category.parentCid = data.category.parentCid
+        this.category.catLevel = data.category.catLevel
+        this.category.sort = data.category.sort
+        this.category.showStatus = data.category.showStatus
+      })
+
+      // this.category.name = data.name
+      // this.catId = data.catId
+    },
+
+    editCategory () {
+      console.log('Edit Category Data: ', this.category)
+      let { catId, name, icon, productUnit } = this.category
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false)
+      }).then(({ data }) => {
+        this.$message({
+          message: 'Edit Successfully',
           type: 'success'
         })
         // Close dialog
